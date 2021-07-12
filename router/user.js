1f2351125ec32194fa6feb71da380a136998c7c2
TaoLoading <1377226380@qq.com>
2021-07-12 17:56:31 +0800

    封装响应结果

--- router/user.js
+++ router/user.js
@@ -1,13 +1,16 @@
 const express = require('express')
+const Result = require('../models/Result')
 
 const router = express.Router()
 
 router.post('/login', function (req, res, next) {
-  console.log('/user/login', req.body)
-  res.json({
-    code: 0,
-    msg: '登录成功'
-  })
+  console.log(req.body)
+  const { username, password } = req.body
+  if (username === 'admin' && password === 'admin') {
+    new Result('登陆成功').success(res)
+  } else {
+    new Result('登陆失败').fail(res)
+  }
 })
 
 router.get('/info', function (req, res, next) {
