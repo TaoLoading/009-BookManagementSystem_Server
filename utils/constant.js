73965ce2e63de52a966733abc4ee38776a102dff
TaoLoading <1377226380@qq.com>
2021-07-14 15:24:28 +0800

    上传图书API

--- utils/constant.js
+++ utils/constant.js
@@ -1,3 +1,6 @@
+// 电子书上传的最终地址，此处为本地
+const UPLOAD_PATH = 'D:/MyProject/009-BookManagementSystem_Server/upload'
+
 module.exports = {
   CODE_ERROR: -1,
   CODE_SUCCESS: 0,
@@ -10,4 +13,5 @@ module.exports = {
   PRIVATE_KEY: 'bookmanagementsystem',
   // token过期时间，此处为一小时
   JWT_EXPIRED: 60 * 60,
+  UPLOAD_PATH
 }
